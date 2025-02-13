da1c4f7e030ac9c384015f22919c8016299a55c7
Campelo <josecampelojr@gmail.com>
2025-02-12 21:42:30 -0300

    Aula de Dados primitivos

---
+++ src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,17 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+            Dados primitivos - int, double, float, char, boolean, short
+            Objetivo da aula: Criar um ninja - Naruto
+         */
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        int saldoBancarioLimitado = 999999999;
+        long saldoBancario = 999999999999999999L;
+    }
+}
